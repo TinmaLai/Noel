f2b0ab169d2743df8cb5f1d9da5f1916bac515ee
Tín Nguyễn <54711104+TinmaLai@users.noreply.github.com>
2023-12-22 20:15:02 +0700

    Update script.js

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ document.addEventListener('DOMContentLoaded', function () {
             var currentTop = parseInt(getComputedStyle(movingElement).top);
             var currentLeft = parseInt(getComputedStyle(movingElement).left);
             if (currentTop < 700) {
-                movingElement.style.top = (currentTop + 7) + 'px';
+                movingElement.style.top = (currentTop + 5) + 'px';
                 movingElement.style.left = currentLeft + 'px';
             } else {
                 isFalling = false;
@@ -42,19 +42,26 @@ document.addEventListener('DOMContentLoaded', function () {
     }
     movingElement.addEventListener('click', function () {
         isStopMoving = true;
-        var appearGiftInterval = setInterval(appearGift, 50);
-        var moveHeart = setInterval(moveDiv, 200);
+        var appearGiftInterval = setInterval(appearGift, 200);
         setTimeout(() => {
-            if (stopMoveHeart) {
-                clearInterval(moveHeart);
-                var moveHearts = document.getElementsByClassName('move-to-heart');
-                for (let i = 0; i < moveHearts.length; i++) {
-                    moveHearts[i].style.display = 'none';
-                }
-                movingElement.style.display = 'none';
-                typeText(0);
+            clearInterval(appearGiftInterval);
+            clearInterval(moveHeart);
+            var hideGiftInterval = setInterval(hideGift, 200);
+            setTimeout(() => {
+                clearInterval(hideGiftInterval);
+            }, 12400);
+        }, 20000);
+        var moveHeart = setInterval(moveDiv, 250);
+        
+        setTimeout(() => {
+                // clearInterval(moveHeart);
+            var moveHearts = document.getElementsByClassName('move-to-heart');
+            for (let i = 0; i < moveHearts.length; i++) {
+                moveHearts[i].style.display = 'none';
             }
-        }, 8000);
+            movingElement.style.display = 'none';
+            typeText(0);
+        }, 35400);
         // if(indexAppear >= 61) {
         //     clearInterval(appearGiftInterval);
         //     setInterval(hideGift, 800);
@@ -67,7 +74,7 @@ document.addEventListener('DOMContentLoaded', function () {
 function appearGift() {
     var gifts = document.getElementsByClassName('text-bind');
     if (indexAppear >= 61) {
-        setInterval(hideGift, 800);
+        // setInterval(hideGift, 800);
     } else {
         gifts[indexAppear++].style.display = 'block';
     }
@@ -104,8 +111,7 @@ function typeText(index) {
     var textToType = `Há nhô, vẫn là anh đây :v\n
     Lúc bé đọc được những dòng này, anh mong bé đang ở trong vòng tay anh (theo kế hoạch là như vậy hehe). Sao nhỉ, anh chỉ muốn nói lời cảm ơn thôi, cảm ơn em đã bước đến và vẫn luôn ở đây,
     cảm ơn em đã rep tin nhắn anh vào hôm đó, cảm ơn vì sự quan tâm ân cần em mang lại, cảm ơn vì những lời nói yêu ta trao nhau vào sáng sớm, cảm ơn những cái ôm, nắm tay, môi hôn thật ngọt ngào từ iêm, cảm ơn vì những niềm vui, và cảm ơn cả những nỗi buồn nữa :v \n
-    Có lẽ cách mà anh xuất hiện không được hợp lí như những mối tình của em trước đây, nhưng anh nghĩ mình gặp nhau đã là duyên rồi, ở lại nữa phải là định mệnh. Trước khi gặp em thực sự anh đã nghĩ hạnh phúc đến từ tình yêu có lẽ không thuộc về mình, "trái tim này đã vụn vỡ từ lâu rồi", anh đã giỡn như vậy phải không :v, nhưng em đã đến và thật nhanh cho anh thấy anh cũng có thể được
-    yêu một người nhiều đến vậy. Tuy cũng có nhiều lúc anh vụng về làm em giận, làm em buồn nhưng đừng quên, anh yêu em, cố hoàn thiện nhất để yêu em, trân trọng từng phút ở bên em (trông thế thôi chứ anh cũng có lúc nhạy cảm, đôi khi cũng biết buồn 1 tí :v) \n
+    Có lẽ cách mà anh xuất hiện không được hợp lí như những mối tình của em trước đây, nhưng anh nghĩ mình gặp nhau đã là duyên rồi, ở lại nữa phải là định mệnh. Trước khi gặp em thực sự anh đã nghĩ hạnh phúc đến từ tình yêu có lẽ không thuộc về mình, "trái tim này đã vụn vỡ từ lâu rồi", anh đã giỡn như vậy phải không :v, nhưng em đã đến và thật nhanh cho anh thấy anh cũng có thể được yêu một người nhiều đến vậy. Tuy cũng có nhiều lúc anh vụng về làm em giận, làm em buồn nhưng đừng quên, anh yêu em, cố hoàn thiện nhất để yêu em, trân trọng từng phút ở bên em (trông thế thôi chứ anh cũng có lúc nhạy cảm, đôi khi cũng biết buồn 1 tí :v) \n
     Văn vở cũng mỏi tay ròi, chúc em cùng gia đình giáng sinh hạnh phúc. \n
     Giáng sinh này, có anh, yêu em.`;
 
